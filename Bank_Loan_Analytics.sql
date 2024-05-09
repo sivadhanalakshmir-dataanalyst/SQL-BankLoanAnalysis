d644ab01cdcaa27c4ce4616e9392407b5df92ffe
SivaDhanaLakshmi Ranimekala <160882996+sivadhanalakshmir-dataanalyst@users.noreply.github.com>
2024-05-09 12:30:10 +0530

    Update Bank_Loan_Analytics.sql

--- Bank_Loan_Analytics.sql
+++ Bank_Loan_Analytics.sql
@@ -1,15 +1,18 @@
-create database Bank_Loan_Analytics;
+CREATE database Bank_Loan_Analytics;
 use Bank_Loan_Analytics;
-select * from finance_001;
-select * from Finance_02;
+SELECT * FROM finance_001;
+SELECT * FROM Finance_02;
+
 
 /*  1.Year wise loan amount status
-	2.Grade and sub grade wise revol_bal
+    2.Grade and sub grade wise revol_bal
     3.Total Payment for verified status Vs Total payment for non-verified status
     4.State wise laon status
     5.Home ownership Vs last payment date stats
 */
-# KPI 1
+
+
+# KPI 1- Find Year Wise loan amount status
 SELECT 
 	year(issue_d) as year_of_issue_d, sum(loan_amnt) as Total_loan_amnt 
 FROM 
@@ -20,9 +23,7 @@ ORDER BY
 	year_of_issue_d;
 
 
-
-
-# KPI 2
+# KPI 2- Find Grade and sub grade wise revol_bal
 SELECT 
 	grade, sub_grade, sum(revol_bal) as total_revol_bal 
 FROM 
@@ -35,7 +36,7 @@ ORDER BY
 	grade,sub_grade;
 
 
-# KPI 3
+# KPI 3- Find Total Payment for verified status Vs Total payment for non-verified status
 SELECT 
 	verification_status,concat("$",format(round(sum(total_pymnt)/1000000,2),2),"M") 
 AS 
@@ -49,8 +50,7 @@ GROUP BY
 
 
 
-
-# KPI 4
+# KPI 4- Find State wise laon status
 SELECT 
 	addr_state, last_Credit_pull_D,loan_status 
 FROM  
@@ -64,10 +64,7 @@ ORDER BY
 
 
 
-
-
-
-# KPI 5
+# KPI 5- Find Home ownership Vs last payment date stats
 SELECT
 	home_ownership, last_pymnt_d, sum(last_pymnt_d) 
 AS
